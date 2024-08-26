211c3c0b79d8443af1f2ad4e2e2c1715cea1087f
TchTech <67865526+TchTech@users.noreply.github.com>
2024-08-26 19:58:36 +0300

    Update kivyapp.py

--- kivyapp.py
+++ kivyapp.py
@@ -5,7 +5,10 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 import serial
 
-def callback(instance):
+def callback1(instance):
+   pass
+
+def callback0(instance):
     ser = serial.Serial('COM1', 9600, timeout=0)
     data = ser.read()
     msg = ""
@@ -26,11 +29,14 @@ class LoginScreen(FloatLayout):
         else:
            msg = "Последнее сообщение: "+str(data)[3:]
         self.add_widget(Label(text=msg, pos_hint={'x':0, 'y':0.2}, font_size=30))
-        btn0 = Button(text="Обновить", size_hint=(.4,.15), pos_hint={'x':0.4, 'y':0.3}, font_size=30)
-        btn0.bind(on_press=callback)
+        btn0 = Button(text="Обновить", size_hint=(.4,.15), pos_hint={'x':0.25, 'y':0.5}, font_size=30)
+        self.add_widget(TextInput(text='Позывной:', pos_hint={'x':0.35, 'y':.1}, size_hint=(.3,.15)))
+        btn0.bind(on_press=callback0)
         self.add_widget(btn0)
-        self.add_widget(Label(text="Отправить сообщение:", pos_hint={'x':0, 'y':0}, font_size=20))
-        self.add_widget(TextInput(text='Hello world', pos_hint={'x':0.2, 'y':.1}, size_hint=(.5,.15)))
+        btn1 = Button(text="Отправить", size_hint=(.4,.15), pos_hint={'x':0.25, 'y':.1},  font_size=30)
+        btn1.bind(on_press=callback1)
+        self.add_widget(btn1)
+        self.add_widget(TextInput(text='Сообщение:', pos_hint={'x':0.3, 'y':0.3}, size_hint=(.3,.15)))
 class MyApp(App):
 
   def build(self):
